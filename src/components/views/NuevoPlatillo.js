76ba35d9b4947c35658ffd9267ca11463bb60a55
victor <vpinedaa@unah.hn>
2022-02-18 16:56:51 -0600

    mostrando msj img

--- src/components/views/NuevoPlatillo.js
+++ src/components/views/NuevoPlatillo.js
@@ -7,9 +7,9 @@ import FileUploader from "react-firebase-file-uploader";
 
 const NuevoPlatillo = () => {
 
-    const [ subiendo, setSubiendo ] = useState(false)
-    const [ progreso, setProgreso ] = useState(0)
-    const [ urlimagen, setUrlimagen] = useState('')
+    const [subiendo, setSubiendo] = useState(false)
+    const [progreso, setProgreso] = useState(0)
+    const [urlimagen, setUrlimagen] = useState('')
 
     //context con operaciones firebase
     const { firebase } = useContext(FirebaseContext)
@@ -73,11 +73,11 @@ const NuevoPlatillo = () => {
 
         //almacenar url
         const url = await firebase
-                    .storage
-                    .ref("productos")
-                    .child(nombre)
-                    .getDownloadURL();
-        
+            .storage
+            .ref("productos")
+            .child(nombre)
+            .getDownloadURL();
+
         console.log(url)
         setUrlimagen(url)
     }
@@ -181,6 +181,23 @@ const NuevoPlatillo = () => {
                                 onProgress={handleProgress}
                             />
                         </div>
+                        { subiendo && (
+                            <div className="h-12 relative w-full border">
+                                <div className="bg-green-500 absolute left-0 top-0 text-white px-2 text-sm h-12 flex items-center" style={{ width: `${progreso}%` }}>
+                                    {progreso} % 
+                                </div>
+                            </div>
+                        ) 
+                        }
+
+                        { urlimagen && (
+                            <p className="bg-green-500 text-white p-3 text-center my-5">
+                                La imagen se subió correctamente
+                            </p>
+                        ) 
+                        }
+
+                        
 
                         <div className='mb-4'>
                             <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='descripcion'>Descripcion</label>
