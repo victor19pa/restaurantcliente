eeaa746f6a9c5c3ba1b9ecb8ccc9a259e387d854
victor <vpinedaa@unah.hn>
2022-02-18 17:19:37 -0600

    acceder a los datos y hacer la consulta

--- src/components/views/Menu.js
+++ src/components/views/Menu.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { FirebaseContext } from '../../firebase'
 
 const Menu = () => {
+    const [platillos, setPlatillos ] = useState([])
+
+    const { firebase } = useContext(FirebaseContext)
+
+    //consultar base de datos mediante snapshot
+    useEffect(() => {
+        const obtenerPlatillos = () => {
+            firebase.db.collection('productos').onSnapshot(handleSnapshot);
+        }
+        obtenerPlatillos()
+    },[])
+
+    //snapshot para usar en tiempo real
+    function handleSnapshot(snapshot){
+        const platillos = snapshot.docs.map(doc => {
+            return{
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+        //guardar los platillos
+        setPlatillos(platillos)
+    }
+    
     return (
         <>
             <h1 className='text-3xl font-light mb-4'>
