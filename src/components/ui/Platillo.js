82666381938d600da1d8cab063913e636ede8a5d
victor <vpinedaa@unah.hn>
2022-02-18 17:51:55 -0600

    disponibilidad de platillos

--- src/components/ui/Platillo.js
+++ src/components/ui/Platillo.js
@@ -10,8 +10,23 @@ const Platillo = ({platillo}) => {
                 <div className='p-5 shadow-md bg-white'>
                     <div className='lg:flex'>
                         <div className='lg:w-5/12 xl:w-3/12'>
+                            
                             <img src={imagen} alt='imagen platillo'/>
+
+                            <div className='sm:flex sm:-mx-2 p-l2'>
+                                <label className='block mt-5 sm:w-2/4'>
+                                    <span className='block text-gray-800 mb-2'>Existencia</span>
+
+                                    <select 
+                                        value={existencia}
+                                        className='bg-white shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline'>
+                                        <option value='true'>Disponible</option>
+                                        <option value='false'>No Disponible</option>
+                                    </select>
+                                </label>
+                            </div>
                         </div>
+
                         <div className='lg:w-7/12 xl:w-9/12 pl-5'>
                             <p className="font-bold text-2xl text-yellow-600 mb-4">{nombre} </p>
                             <p className="text-gray-600 mb-4">
