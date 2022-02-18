83527d864d6bcc6da5cf2f6678ae3ff106285269
victor <vpinedaa@unah.hn>
2022-02-18 09:35:00 -0600

    estilos, finalizando sidebar y routin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
+import Sidebar from './components/ui/Sidebar';
 import Menu from './components/views/Menu';
 import NuevoPlatillo from './components/views/NuevoPlatillo';
 import Ordenes from './components/views/Ordenes';
@@ -8,11 +9,16 @@ import Ordenes from './components/views/Ordenes';
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Ordenes/>}/>
-        <Route path='/menu' element={<Menu/>}/>
-        <Route path='/nuevo-platillo' element={<NuevoPlatillo/>}/>
-      </Routes>
+      <div className='md:flex min-h-screen'>
+      <Sidebar />        
+        <div className='md:w-3/5 xl:w-4/5'>
+          <Routes>
+            <Route path='/' element={<Ordenes/>}/>
+            <Route path='/menu' element={<Menu/>}/>
+            <Route path='/nuevo-platillo' element={<NuevoPlatillo/>}/>
+          </Routes>
+        </div>
+      </div>
     </>
   );
 }
