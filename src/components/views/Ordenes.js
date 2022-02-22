db716d4841c79fe64fa60dba6eb3284fe06d969b
victor <vpinedaa@unah.hn>
2022-02-21 19:19:28 -0600

    consulta para ordenes no completadas

--- src/components/views/Ordenes.js
+++ src/components/views/Ordenes.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import FirebaseContext from '../../firebase/context';
+
 const Ordenes = () => {
+
+    //context con las operaciones de firebase
+    const { firebase } = useContext(FirebaseContext);
+
+    //state ordenes
+    const [ ordenes, setOrdenes ] = useState([])
+    
+    useEffect(()=>{
+        const obtenerOrdenes = () =>{
+            firebase.db.collection('ordenes').where('completado',"==", false).onSnapshot(manejarSnapshot)
+        }
+        obtenerOrdenes()
+    },[])
+
+    function manejarSnapshot(snapshot){
+        const ordenes = snapshot.docs.map(doc => {
+            return{
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+
+        setOrdenes(ordenes)
+    }
     return (
         <>
             <h1 className='text-3xl font-light mb-4'>
